5ecd5c2a7f9459b722d4cc448def72a07eed03e7
wwwking837 <muhammadzaroonadnan@gmail.com>
2025-03-05 20:23:07 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+print("Welcome to Python class")
+print("4")
+# Acts as a string 
+print(4)
+# Acts as a integer
+print("\n")
+print("Below are keywords which are imported")
+import keyword
+print(keyword.kwlist)
